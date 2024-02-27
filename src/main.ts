73d4dc2469e1c3069654e0bbc9a7768ecf4329c0
spacemeowx2 <spacemeowx2@gmail.com>
2024-02-27 16:46:57 +0800

    feat: getCenterMessage

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,28 @@ import { cv } from 'opencv-wasm';
 import { readFile } from 'fs/promises'
 import Jimp from 'jimp'
 
-const img = await readFile("./testimg/S3.jpg");
-
 const worker = await createWorker('chi_sim', OEM.DEFAULT, {
-    langPath: "./data/",
-    cachePath: "./cache/",
-    logger: (m: unknown) => console.log(m),
+  langPath: "./data/",
+  cachePath: "./cache/",
+  logger: (m: unknown) => console.log(m),
 });
 
 await worker.load();
 
-const { data } = await worker.recognize(img);
-console.log(data.text);
+async function getCenterMessage(buffer: Buffer) {
+  const img = await Jimp.create(buffer);
+  const centerImg = await img
+    .resize(1280, 720)
+    .crop(400, 300, 400, 120)
+    .getBufferAsync(Jimp.MIME_JPEG);
+
+  const { data } = await worker.recognize(centerImg);
+
+  return data.text;
+}
+
+const img = await readFile("./testimg/IMG_4622.JPG");
+
+console.log(await getCenterMessage(img));
+
 await worker.terminate();
