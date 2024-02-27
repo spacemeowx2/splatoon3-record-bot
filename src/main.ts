7f547f6ceb1fd0774609b38faf5c2f09c91825ff
spacemeowx2 <spacemeowx2@gmail.com>
2024-02-27 16:18:54 +0800

    refactor: upgrade tesseract.js

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
-import { createWorker } from "tesseract.js";
+import { createWorker, OEM } from "tesseract.js";
 import { cv } from 'opencv-wasm';
 import { readFile } from 'fs/promises'
 import Jimp from 'jimp'
 
 const img = await readFile("./testimg/S3.jpg");
 
-const worker = await createWorker({
+const worker = await createWorker('chi_sim', OEM.DEFAULT, {
     langPath: "./data/",
     cachePath: "./cache/",
     logger: (m: unknown) => console.log(m),
 });
 
 await worker.load();
-await worker.loadLanguage("chi_sim");
-await worker.initialize("chi_sim");
 
 const { data } = await worker.recognize(img);
 console.log(data.text);
